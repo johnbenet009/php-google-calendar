1d2e3c698709f03431da9652a3e3d036f6cdf821
Positive John <johnbenet0009@gmail.com>
2025-01-09 21:50:57 +0100

    Update demo.php

--- demo.php
+++ demo.php
@@ -1,12 +1,12 @@
 <?php 
 $payload = json_encode([
-    "date" => "2025-01-09",
+    "date" => "2025-01-10",
     "time" => "09:00",
-    "summary" => "Client Meeting",
+    "summary" => "Local Host Booking",
     "description" => "Meeting to discuss project."
 ]);
 
-$ch = curl_init("http://localhost/ExpTreasure/SmallAdmin/calendar.php");
+$ch = curl_init("http://your-website.com/calendar.php");
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_POST, true);
 curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
